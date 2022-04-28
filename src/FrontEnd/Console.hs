e9b844c10a920bc7130c3998d21731f00c9db103
GololobovAlexander <gololobovalex12@gmail.com>
2022-04-28 11:30:43 +0300

    console (need fixes for non int type of input after /repeat)

--- src/FrontEnd/Console.hs
+++ src/FrontEnd/Console.hs
@@ -12,15 +12,33 @@ where
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified EchoBot
+import Data.Maybe (fromMaybe)
 
 newtype Handle = Handle
   { hBotHandle :: EchoBot.Handle IO T.Text
   }
 
 run :: Handle -> IO ()
-run _ = do
-  TIO.putStrLn "Welcome to the echo-bot!"
+run h = do
+  TIO.putStrLn "Please input message, /help or /repeat command"
   -- 1. Read a line from the console.
+  line <- getLine
   -- 2. Send it to the bot, get its response and output it.
+  let handle = hBotHandle h
+  let message = EchoBot.hMessageFromText handle (T.pack line)
+  responses <- EchoBot.respond handle (EchoBot.MessageEvent message)
+  let getR [] = putStrLn ""
+      getR ((EchoBot.MessageResponse a) : xs) = TIO.putStrLn a >> getR xs
+      getR ((EchoBot.MenuResponse _ xs) : _) = do
+        putStrLn "Input the number of repeatitions"
+        newCount <- getLine
+        let nc = read newCount
+        let newEvent = fromMaybe (EchoBot.MessageEvent "") $ lookup nc xs
+        _ <- EchoBot.respond handle newEvent
+        putStrLn ""
+  getR responses
+  --let texts = map (getR handle) responses
+  --printList texts
   -- 3. Go to 1.
-  error "Not implemented"
+  run h
+  -- error "Not implemented"
