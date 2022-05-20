6cb72852740900224674ce0ea9cb965d1db4a186
GololobovAlexander <gololobovalex12@gmail.com>
2022-05-20 20:54:38 +0300

    json for keyboard & fixes

--- src/JSONparsing.hs
+++ src/JSONparsing.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module JSONparsing where
 
 import Control.Monad (MonadPlus (mzero))
-import Data.Aeson ( (.:), (.:?), FromJSON(parseJSON), Value(Object) )
+import Data.Aeson ( (.:), (.:?), FromJSON(parseJSON), Value(Object), ToJSON, toJSON, object, KeyValue ((.=)), toEncoding, pairs)
 import GHC.Generics ( Generic )
 
 data TelegramResponse = TelegramResponse { 
@@ -18,11 +20,11 @@ instance FromJSON TelegramResponse where
 
 data Result = Result { 
     update_id :: Integer,
-    message   :: Message
+    message   :: Maybe Message
 } deriving (Show, Generic)
 
 instance FromJSON Result where
-  parseJSON (Object v) = Result <$> v .: "update_id" <*> v .: "message"
+  parseJSON (Object v) = Result <$> v .: "update_id" <*> v .:? "message"
   parseJSON _ = mzero
 
 data Message = Message { 
@@ -30,7 +32,7 @@ data Message = Message {
     from       :: From,
     chat       :: Chat,
     date       :: Integer,
-    text       :: Maybe String
+    incoming_text       :: Maybe String
 } deriving (Show, Generic)
 
 instance FromJSON Message where
@@ -41,23 +43,38 @@ instance FromJSON Message where
 data From = From {
   from_id         :: Integer,
   is_bot          :: Bool,
-  from_first_name :: String,
+  from_first_name :: Maybe String,
   from_username   :: Maybe String,
   language_code   :: Maybe String
 } deriving (Show, Generic)
 
 instance FromJSON From where
-  parseJSON (Object v) = From <$> v .: "id" <*> v .: "is_bot" <*> v .: "first_name" <*> v 
+  parseJSON (Object v) = From <$> v .: "id" <*> v .: "is_bot" <*> v .:? "first_name" <*> v 
                                                   .:? "username" <*> v .:? "language_code"
   parseJSON _ = mzero
 
 data Chat = Chat {
-  id :: Maybe Integer,
-  first_name :: String,
-  username   :: String,
+  cid :: Maybe Integer,
+  first_name :: Maybe String,
+  username   :: Maybe String,
   chat_type  :: Maybe String
 } deriving (Show, Generic)
 
 instance FromJSON Chat where
-  parseJSON (Object v) = Chat <$> v .:? "id" <*> v .: "first_name" <*> v .: "username" <*> v .:? "chat_type"
+  parseJSON (Object v) = Chat <$> v .:? "id" <*> v .:? "first_name" <*> v .:? "username" <*> v .:? "chat_type"
   parseJSON _ = mzero
+
+data Button = Button {
+  text :: String,
+  callback_data :: String
+  } deriving (Show, Generic)
+
+instance ToJSON Button where
+  toEncoding (Button text callback_data) =
+    pairs ("text" .= text <> "callback_data" .= callback_data)
+
+newtype Inline = Inline {
+  inline_keyboard :: [[Button]]
+} deriving (Show, Generic)
+
+instance ToJSON Inline where 
